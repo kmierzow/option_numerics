8eef6677c14594190c345349a6feecb5c4bcf900
kmierzow <krzysztofmierzowski@eduroam-077-080-096-033.wlan.univie.ac.at>
2025-10-28 14:17:25 +0100

    updating the app to work with streamlit

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 import os
+import sys
 import pybind11
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 
-os.environ["CC"] = "gcc"
-os.environ["CXX"] = "g++"
-
-extra_compile_args = [
-    "-O3",
-    "-fopenmp",
-    "-DEIGEN_VECTORIZE",
-    "-march=x86-64",
-]
-
-extra_link_args = [
-    "-fopenmp",
-]
+# Platform-specific compilation flags
+if sys.platform == "darwin":  # macOS
+    extra_compile_args = [
+        "-O3",
+        "-fopenmp",
+        "-DEIGEN_VECTORIZE",
+        "-arch", "arm64" if os.uname().machine == "arm64" else "x86_64"
+    ]
+    extra_link_args = [
+        "-fopenmp",
+        "-lomp"
+    ]
+    eigen_include = "/opt/homebrew/include/eigen3" if os.path.exists("/opt/homebrew/include/eigen3") else "/usr/local/include/eigen3"
+else:  # Linux (Docker/cloud)
+    extra_compile_args = [
+        "-O3",
+        "-fopenmp",
+        "-DEIGEN_VECTORIZE"
+    ]
+    extra_link_args = [
+        "-fopenmp"
+    ]
+    eigen_include = "/usr/include/eigen3"
 
 ext_modules = [
     Extension(
@@ -23,9 +34,8 @@ ext_modules = [
         ["numerical_methods.cpp"],
         include_dirs=[
             pybind11.get_include(),
-            "/usr/include/eigen3",
+            eigen_include
         ],
-        library_dirs=[],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
         language="c++",
@@ -37,4 +47,6 @@ setup(
     version="0.1",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
+
